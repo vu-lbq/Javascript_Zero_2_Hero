8aa2586407dce9412b7f0adf44c2cdbe4a20ac8c
Vu Le Bui Quoc <vu.le.bui.quoc@gmail.com>
2024-02-15 16:37:16 +0700

    Update leetcode first day

---
+++ Leet_code_JS30days/Closures/hello_world_function.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return () => "Hello World"
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+/**
+ * input []
+ * output "Hello World"
+ * 
+ * input [{},null,42]
+ * output "Hello World"
+ */
