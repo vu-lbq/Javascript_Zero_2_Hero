5c6f78eea429c2a5f5e30d21e6507268740e8727
Vu Le Bui Quoc <vu.le.bui.quoc@gmail.com>
2024-02-16 22:59:59 +0700

    Complete 03_Function_Transformations

--- Leet_code_JS30days/03_Function_Transformations/memoize_Catching.js
+++ Leet_code_JS30days/03_Function_Transformations/memoize_Catching.js
@@ -3,3 +3,52 @@ Given a function fn, return a memoized version of that function.
 A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
  */
 
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    
+    const cache = new Map(); 
+
+    return function(...args) {
+        const key = JSON.stringify(args); 
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        } else {
+            const result = fn(...args);
+            cache.set(key, result);
+            return result;
+        }
+    };
+}
+
+function memoize(fn) {
+    const cache = {};
+    
+    return function(...args) {
+      const key = JSON.stringify(args);
+      
+      if (cache[key] !== undefined) {
+        return cache[key];
+      }
+      
+      const result = fn(...args);
+      cache[key] = result;
+      
+      return result;
+    };
+  }
+  
+  
+  /** 
+   * let callCount = 0;
+   * const memoizedFn = memoize(function (a, b) {
+   *	 callCount += 1;
+   *   return a + b;
+   * })
+   * memoizedFn(2, 3) // 5
+   * memoizedFn(2, 3) // 5
+   * console.log(callCount) // 1 
+   */
